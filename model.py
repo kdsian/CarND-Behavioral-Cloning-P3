d7a81cac004c152885648cdd99ad0c0b03251f38
Hyungil kang <smrksd@gmail.com>
2018-01-03 23:20:33 +0900

    sub lecture 13
    sub lecture 13

--- model.py
+++ model.py
@@ -10,14 +10,21 @@ with open('data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+correction = 0.2
 for line in lines:
-    source_path=line[0]
-    filename = source_path.split('\\')[-1]
-    current_patch='data/IMG/'+filename
-    image=cv2.imread(current_patch)
-    images.append(image)
-    measurement=float(line[3])
-    measurements.append(measurement)
+    for i in range(3):
+        source_path=line[i]
+        filename = source_path.split('\\')[-1]
+        current_patch='data/IMG/'+filename
+        image=cv2.imread(current_patch)
+        images.append(image)
+
+        measurement=float(line[3])
+        if i==1:
+            measurement+=correction
+        elif i==2:
+            measurement-=correction
+        measurements.append(measurement)
 
 augmented_images  = []
 augmented_measurements = []
@@ -32,19 +39,21 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0-0.5,input_shape=(160,320,3)))
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
