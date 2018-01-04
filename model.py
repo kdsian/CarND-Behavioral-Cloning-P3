19d749efeccfb445ebdabe05537c44d4d20aa53d
Hyungil kang <smrksd@gmail.com>
2018-01-04 21:56:10 +0900

    v1
    v1

--- model.py
+++ model.py
@@ -1,30 +1,108 @@
 import csv
 import cv2
 import numpy as np
+import os
 
-lines = []
-with open('data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+def getLinesFromDrivingLogs(dataPath):
+    """
+    Returns the lines from a driving log with base directory `dataPath`.
+    If the file include headers, pass `skipHeader=True`.
+    """
+    lines = []
+    with open(dataPath+'/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+    return lines        
+
+def findImages(dataPath):
+    """
+    Finds all the images needed for training on the path `dataPath`.
+    Returns `([centerPaths], [leftPath], [rightPath], [measurement])`
+    """
+    directories = [x[0] for x in os.walk(dataPath)]
+    dataDirectories = list(filter(lambda directory: os.path.isfile(directory + '/driving_log.csv'), directories))
+    centerTotal = []
+    leftTotal = []
+    rightTotal = []
+    measurementTotal = []
+    for directory in dataDirectories:
+        lines = getLinesFromDrivingLogs(directory)
+        center = []
+        left = []
+        right = []
+        measurements = []
+        for line in lines:
+            measurements.append(float(line[3]))
+            center.append(directory + '/IMG/' + line[0].split('\\')[-1])
+            left.append(directory + '/IMG/' + line[1].split('\\')[-1])
+            right.append(directory + '/IMG/' + line[2].split('\\')[-1])
+        centerTotal.extend(center)
+        leftTotal.extend(left)
+        rightTotal.extend(right)
+        measurementTotal.extend(measurements)
+
+    return (centerTotal, leftTotal, rightTotal, measurementTotal)
+
+def combineImages(center, left, right, measurement, correction):
+    """
+    Combine the image paths from `center`, `left` and `right` using the correction factor `correction`
+    Returns ([imagePaths], [measurements])
+    """
+    imagePaths = []
+    imagePaths.extend(center)
+    imagePaths.extend(left)
+    imagePaths.extend(right)
+    measurements = []
+    measurements.extend(measurement)
+    measurements.extend([x + correction for x in measurement])
+    measurements.extend([x - correction for x in measurement])
+    return (imagePaths, measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
+
+def createPreProcessingLayers():
+    """
+    Creates a model with the initial pre-processing layers.
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
+    return model
+
+def nVidiaModel():
+    """
+    Creates nVidea Autonomous Car Group model
+    """
+    model = createPreProcessingLayers()
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
+
+# Reading images locations.
+centerPaths, leftPaths, rightPaths, measurements = findImages('data')
+imagePaths, measurements = combineImages(centerPaths, leftPaths, rightPaths, measurements, 0.2)
+print('Total Images: {}'.format( len(imagePaths)))
 
 images = []
-measurements = []
-correction = 0.2
-for line in lines:
-    for i in range(3):
-        source_path=line[i]
-        filename = source_path.split('\\')[-1]
-        current_patch='data/IMG/'+filename
-        image=cv2.imread(current_patch)
-        images.append(image)
+for imagePath in imagePaths:
+    image=cv2.imread(imagePath)
+    images.append(image)
 
-        measurement=float(line[3])
-        if i==1:
-            measurement+=correction
-        elif i==2:
-            measurement-=correction
-        measurements.append(measurement)
+
+
 
 augmented_images  = []
 augmented_measurements = []
@@ -38,26 +116,23 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
+# Model creation
+model = nVidiaModel()
 
-model = Sequential()
-model.add(Lambda(lambda x:x/255.0-0.5,input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(1))
-
+# Compiling and training the model
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True,nb_epoch=5)
+history_object=model.fit(X_train, y_train, validation_split=0.2, shuffle=True,nb_epoch=3,verbose=1)
 
 model.save('model.h5')
-exit()
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
