8a1b62e293e828bb91cb5ddad87de1dfa615cdc2
Hyungil kang <smrksd@gmail.com>
2018-01-04 22:43:00 +0900

    v2
    v2

--- model.py
+++ model.py
@@ -59,6 +59,34 @@ def combineImages(center, left, right, measurement, correction):
     measurements.extend([x - correction for x in measurement])
     return (imagePaths, measurements)
 
+import sklearn
+
+def generator(samples, batch_size=32):
+    """
+    Generate the required images and measurments for training/
+    `samples` is a list of pairs (`imagePath`, `measurement`).
+    """
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        samples = sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for imagePath, measurement in batch_samples:
+                image = cv2.imread(imagePath)
+                images.append(image)
+                angles.append(measurement)
+                # Flipping
+                images.append(cv2.flip(image,1))
+                angles.append(measurement*-1.0)
+
+            # trim image to only see section with road
+            inputs = np.array(images)
+            outputs = np.array(angles)
+            yield sklearn.utils.shuffle(inputs, outputs)
+
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
@@ -96,33 +124,25 @@ centerPaths, leftPaths, rightPaths, measurements = findImages('data')
 imagePaths, measurements = combineImages(centerPaths, leftPaths, rightPaths, measurements, 0.2)
 print('Total Images: {}'.format( len(imagePaths)))
 
-images = []
-for imagePath in imagePaths:
-    image=cv2.imread(imagePath)
-    images.append(image)
+# Splitting samples and creating generators.
+from sklearn.model_selection import train_test_split
+samples = list(zip(imagePaths, measurements))
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+print('Train samples: {}'.format(len(train_samples)))
+print('Validation samples: {}'.format(len(validation_samples)))
 
-
-
-augmented_images  = []
-augmented_measurements = []
-
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+train_generator = generator(train_samples, batch_size=64)
+validation_generator = generator(validation_samples, batch_size=64)
 
 # Model creation
 model = nVidiaModel()
 
 # Compiling and training the model
 model.compile(loss='mse', optimizer='adam')
-history_object=model.fit(X_train, y_train, validation_split=0.2, shuffle=True,nb_epoch=3,verbose=1)
-
+history_object = model.fit_generator(train_generator, samples_per_epoch= len(train_samples), 
+validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3, 
+verbose=1)
 model.save('model.h5')
 
 ### print the keys contained in the history object
